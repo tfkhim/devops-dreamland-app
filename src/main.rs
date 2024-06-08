81a6030153c685fc252cb6ac6db5854c27b4ba3f
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2024-06-08 22:16:06 +0200

    build(deps): update all crate version

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use axum::{routing::get, Router};
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
-        .serve(app.into_make_service())
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
         .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
