dcbcf3d4a8733df0566dbf120ae713116af68a48
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2024-06-09 11:12:00 +0200

    feat: greet the given person

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::io;
 
-use axum::{routing::get, Router};
+use axum::{extract::Path, routing::get, Router};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/greetings/:name", get(greet));
 
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
     axum::serve(listener, app).await
+}
+
+async fn greet<'de>(Path(name): Path<String>) -> String {
+    format!("Hello, {}!", name)
 }
