cd941e7bc60a0c504cfa5c0d2627901ea8c28c5d
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2024-06-09 10:03:36 +0200

    refactor: return a Result instead of panicking

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::io;
+
 use axum::{routing::get, Router};
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> io::Result<()> {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
-        .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    axum::serve(listener, app).await
 }
